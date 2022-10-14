f45416df77e69b732d279763ef5da8f333e9b419
AstraMtz <92556618+AstraMtz@users.noreply.github.com>
2022-10-14 17:20:28 -0500

    create sum_of_digits_with_loop.cbl

---
+++ 8kyu/cobol/cobol_sum_of_digits/sum_of_digits_with_loop.cbl
@@ -0,0 +1,24 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. DIGITAL-ROOT.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 SUM-DIG         PIC 9(10).
+       LINKAGE SECTION.
+      * Input :
+       01 NUMB      PIC 9(10).
+      * Output :
+       01 ROOT      PIC 9.
+       PROCEDURE DIVISION USING NUMB ROOT.
+      * Create DIGITAL-ROOT to compute digital root
+         IF NUMB < 10
+            MOVE NUMB TO ROOT
+         ELSE
+            MOVE 0 TO SUM-DIG
+            PERFORM UNTIL NUMB = 0
+                DIVIDE NUMB BY 10 GIVING NUMB REMAINDER ROOT
+                ADD ROOT TO SUM-DIG
+            END-PERFORM
+            MOVE SUM-DIG TO NUMB
+            CALL 'DIGITAL-ROOT' USING NUMB ROOT
+         END-IF.
+       END PROGRAM DIGITAL-ROOT.
