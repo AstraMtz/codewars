cc557bd1d4b856fd82dabc49e4f18f617d9ea9e6
AstraMtz <92556618+AstraMtz@users.noreply.github.com>
2022-10-14 17:16:35 -0500

    create sum_of_digits_with_formula_math.cbl

---
+++ 8kyu/cobol/cobol_sum_of_digits/sum_of_digits_with_formula_math.cbl
@@ -0,0 +1,13 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. DIGITAL-ROOT.
+       DATA DIVISION.
+       LINKAGE SECTION.
+      * Input :
+       01 NUMB      PIC 9(10).
+      * Output :
+       01 ROOT      PIC 9.
+       PROCEDURE DIVISION USING NUMB ROOT.
+      * Create DIGITAL-ROOT to compute digital root
+       IF NUMB = ZERO THEN COMPUTE ROOT = 0.
+       IF NUMB > 0 THEN COMPUTE ROOT = FUNCTION MOD(NUMB - 1, 9) + 1.
+       END PROGRAM DIGITAL-ROOT.
