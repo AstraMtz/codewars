36b732c6b1b4f3784a93625045bdf3a5d7f6d04e
AstraMtz <92556618+AstraMtz@users.noreply.github.com>
2022-10-14 16:59:13 -0500

    sum_of_digits_with_recursion.clj

---
+++ 8kyu/clojure/clojure_sum_of_digits/sum_of_digits_with_recursion.clj
@@ -0,0 +1,8 @@
+(ns digital-root)
+
+(defn digital-root [number]
+  (if (< number 10)
+    number
+    (digital-root (+ (mod number 10)
+                     (digital-root (quot number 10))))) ; quot is //
+)
